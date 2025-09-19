402679c56302f1894c8ae65b852d134296a04749
lrtraviteja <lrtraviteja@gmail.com>
2025-09-19 22:22:59 +0530

    add delete, like features with responsiveness to the cards.

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -1,15 +1,34 @@
 import UserCard from '../UserCard/UserCard';
 import { useUsers } from '../../context/UserContext';
 import './UserList.css';
+import { Row, Col } from 'antd';
 
-function UserList() {
-  const { users } = useUsers();
+function UserList({ onEdit }) {
+  const { users, setUsers } = useUsers();
+
+  const handleDelete = (userId) => {
+    setUsers(users.filter(user => user.id !== userId));
+  };
+
+  const handleLike = (userId) => {
+    setUsers(users.map(user =>
+      user.id === userId ? { ...user, liked: !user.liked } : user
+    ));
+  };
+
   return (
-    <div className="user-list-container">
+    <Row gutter={[16, 16]} className="user-list-container">
       {users.map(user => (
-        <UserCard key={user.id} user={user} />
+        <Col key={user.id} xs={24} sm={12} md={8} lg={6}>
+          <UserCard
+            user={user}
+            onEdit={onEdit}
+            onDelete={handleDelete}
+            onLike={handleLike}
+          />
+        </Col>
       ))}
-    </div>
+    </Row>
   );
 }
 
